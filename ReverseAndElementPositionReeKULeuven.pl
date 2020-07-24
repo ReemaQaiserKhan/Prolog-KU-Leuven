54cd385c27c07967638a49e8d12eb53dc6279238
Reema Qaiser Khan <61238811+ReemaQaiserKhan@users.noreply.github.com>
2020-07-24 22:48:21 +0200

    Update ReverseAndElementPositionReeKULeuven.pl

--- ReverseAndElementPositionReeKULeuven.pl
+++ ReverseAndElementPositionReeKULeuven.pl
@@ -1,5 +1,5 @@
-%% You can run ReverseAndElementPositionReeKULeuven.pll on https://swish.swi-prolog.org/. Write ReverseAndElementPositionReeKULeuven.pl in the search bar and then select.
-Q. Create a program that first reverses a list and then gives element in the list with provided position.
+% You can run ReverseAndElementPositionReeKULeuven.pll on https://swish.swi-prolog.org/. Write ReverseAndElementPositionReeKULeuven.pl in the search bar and then select.
+% Q. Create a program that first reverses a list and then gives element in the list with provided position.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % reverse predicate:- reverse a list
 % reverse([1,2,3],Z,[]).
@@ -21,10 +21,10 @@ element_at(X,[_|L],K):-
  reverse([H|T],O,[]),
  element_at(E,O,P).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-Executing Program:
+/*Executing Program:
 ?- main_predicate(E,[1,e,3,s,5],O,4)
 OUTPUT:- 
 E = e,
 O = [5, s, 3, e, 1]
 false
-       
+*/       
