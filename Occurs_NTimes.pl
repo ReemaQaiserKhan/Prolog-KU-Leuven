6c869b71721938217ceb3d422e89f831fc476373
Reema Qaiser Khan <61238811+ReemaQaiserKhan@users.noreply.github.com>
2020-12-03 12:05:25 +0100

    Create Occurs_NTimes.pl

---
+++ Occurs_NTimes.pl
@@ -0,0 +1,37 @@
+/*Write a prolog program that writes how many times an element occurs in a list. Use clpfd.
+*/
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:-use_module(library(clpfd)).
+
+occur_ntimes(X,[H|T],N):-
+    occur_ntimes1(X,[H|T],R),
+    sum(R,N).
+
+occur_ntimes1(_,[],[]).
+occur_ntimes1(X,[H|T],[N|R]):-
+    member(X,[H]),
+    N #= 1+0,
+    occur_ntimes1(X,T,R).
+occur_ntimes1(X,[H|T],R):-
+    \+member(X,[H]),
+    occur_ntimes1(X,T,R).
+
+sum([],0).
+sum([_|T],S):-
+    sum(T,S1),
+    S #= S1+1.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/*Execute the Program:-
+?-occur_ntimes(4,[2,4,4,4,4,4,3,2,2],N).
+OUTPUT:
+N = 5
+false
+?-occur_ntimes(3,[2,4,4,4,4,4,3,2,2],N).
+OUTPUT:
+N = 1
+false
+?-occur_ntimes(2,[2,4,4,4,4,4,3,2,2],N).
+OUTPUT:
+N = 3
+false
+*/
