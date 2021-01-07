cc15b0ad9d4587a2272d80eae5a893114e7a2296
Reema Qaiser Khan <61238811+ReemaQaiserKhan@users.noreply.github.com>
2021-01-07 19:24:41 +0100

    Update revertLastThree.pl

--- revertLastThree.pl
+++ revertLastThree.pl
@@ -7,29 +7,28 @@ revertLastThree([H|T],List):-
     length([H|T],N),
     N1 is N-3,
     revertLastThree2([H|T],N1,L,Rest),
-    sort(Rest,R1),
+   reverse(Rest,R1),
     append(L,R1,List).
 
 revertLastThree2(L,0,[],L).
 revertLastThree2([H|T],N,[H|L],Rest):-
     N1 is N-1,
     revertLastThree2(T,N1,L,Rest).
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 /* Execute the Program:
-?-revertLastThree([2,3,4,7,5,8],List).
+?-rrevertLastThree([a,b,c,d,e,f],List).
 OUTPUT:
-List = [2, 3, 4, 5, 7, 8]
+List = [a, b, c, f, e, d]
 false
 
 ?-revertLastThree([55,84,36,9,77,96,66],List).
 OUTPUT:
-List = [55, 84, 36, 9, 66, 77, 96]
+List = [55, 84, 36, 9, 66, 96, 77]
 false
 
 ?-revertLastThree([55,84,36],List).
 OUTPUT:
-List = [36, 55, 84]
+List = [36, 84, 55]
 false
 
 ?-revertLastThree([55,84],List).
